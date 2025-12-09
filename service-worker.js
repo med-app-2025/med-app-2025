e950093127b93b02eeaf50fe52c1c6aa6f2d1f85
med-app-2025 <taif@uowasit.edu.iq>
2025-12-09 21:27:47 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='attendance-pwa-v1';const FILES_TO_CACHE=['./','index.html','style.css','app.js','fake_map.jpg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
